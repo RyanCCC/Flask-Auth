83309a0e021082abb7419d703ef1033dc4491436
chenjl <chenjl@augurit.com>
2021-09-01 10:59:24 +0800

    add OAuth2

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ REDIS_PORT = '6379'
 REDIS_PASSWORD = '****'
 
 
+PGSQL_CONNECTSTRING = 'dbname=agcimaidb user=agcimai password=agcimai6a35 host=192.168.4.235 port=5432'
+
+REDIS_HOST = '192.168.4.235'
+REDIS_PORT = '6379'
+REDIS_PASSWORD = 'agcimai6a35'
+
