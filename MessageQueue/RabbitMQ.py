623788f85e0b191635c6a2c63a197f7292653140
chenjl <chenjl@augurit.com>
2022-01-07 11:51:23 +0800

    优化rabbitmq

--- MessageQueue/RabbitMQ.py
+++ MessageQueue/RabbitMQ.py
@@ -7,23 +7,57 @@ user = config.MQ_USER
 pwd = config.MQ_PASSWORD
 
 class RabbitProducer():
-    def __init__(self):
-        pass
+    def __init__(self, host, port, queue, user, password, *args, **kwargs):
+        self._host=host
+        self._port=port
+        self._queue=queue
+        self._user=user
+        self._password=password
+        if 'virtual_host' in kwargs.keys():
+            self._virtual_host = kwargs['virtual_host']
+        else:
+            self._virtual_host = '/'
+    
+    def connection(self):
+        credentials = pika.PlainCredentials(self._user, self._password)
+        self.__connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self._host, port=self._port, virtual_host=self._virtual_host, credentials=credentials)
+        )
+        return self.__connection.is_open
+    
+    def publish_message(self,message, exchange =''):
+        channel = self.__connection.channel()
+        result = channel.queue_declare(queue=self._queue)
+        channel.basic_publish(exchange=exchange, routing_key=self._queue, body=message)
+        self.__connection.close()
+
         
 
 class RabbitComsumer():
-    def __init__(self):
-        pass
-
-
-def publish_message(MQ_INFO, message):
-    credentials = pika.PlainCredentials(MQ_INFO['user'],MQ_INFO['password'])
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=MQ_INFO['host'], port=MQ_INFO['port'], virtual_host='/', credentials=credentials)
-    )
-    channel = connection.channel()
-    # 声明消息队列
-    result = channel.queue_declare(queue=MQ_INFO['queue'])
-    channel.basic_publish(exchange='', routing_key=MQ_INFO['queue'], body=message)
-    connection.close()
-
+    def __init__(self, host, port, queue, user, password, *args, **kwargs):
+        self._host=host
+        self._port=port
+        self._queue=queue
+        self._user=user
+        self._password=password
+        if 'virtual_host' in kwargs.keys():
+            self._virtual_host = kwargs['virtual_host']
+        else:
+            self._virtual_host = '/'
+    
+    def connection(self):
+        credentials = pika.PlainCredentials(self._user, self._password)
+        self.__connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self._host, port=self._port, virtual_host=self._virtual_host, credentials=credentials)
+        )
+        return self.__connection is None
+    
+    def subscribe(self):
+        channel = self.__connection.channel()
+        channel.queue_declare(queue=self._queue, durable=False)
+        # 设置回调函数
+        def callback(ch, method,properties, body):
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+            print('receive:'+body.decode())
+        channel.basic_consume(self._queue, callback)
+        channel.start_consuming()
